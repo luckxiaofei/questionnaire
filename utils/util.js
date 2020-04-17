f441a025325eef5e33c929538990b534da5d6922
chenfei <luckxiaofei@outlook.com>
2020-04-17 14:33:07 +0800

    1.0

--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,35 @@
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+
+function isNotBlank(value) {
+    if (value == null || value == undefined || value == '') {
+        return false
+    }
+    return true;
+}
+
+function isBlank(value) {
+    if (value == null || value == undefined || value == '') {
+        return true
+    }
+    return false;
 }
 
 module.exports = {
-  formatTime: formatTime
+    formatTime: formatTime,
+    isNotBlank: isNotBlank,
+    isBlank: isBlank
 }
