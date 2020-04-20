46116a7fb633e09fada8465b8daeb546a0157e95
chenfei <luckxiaofei@outlook.com>
2020-04-20 11:34:10 +0800

    1.0

--- pages/result/result.js
+++ pages/result/result.js
@@ -7,8 +7,10 @@ Page({
   data: {
     score: 0,
     grade: 0,
-    gradeName: '',
-    activeNames: ['1', '2', '3']
+    gradeName: '',//年级
+    interfereType:'',//干预程度
+    recommendedPackage:'',//推荐套餐
+    activeNames: ['1', '2', '3', '4', '5', '6']
   },
 
   /**
@@ -29,27 +31,37 @@ Page({
     if (score > 0 && score <= 120) {
       this.setData({
         grade: 1,
-        gradeName:'一年级'
+        gradeName:'一年级',
+        interfereType: '需极重度干预',
+        recommendedPackage: '自闭症一年级服务套餐'
       })
     } else if (score > 120 && score <= 160) {
       this.setData({
         grade: 2,
-        gradeName: '二年级'
+        gradeName: '二年级',
+        interfereType: '需重度干预',
+        recommendedPackage: '自闭症二年级服务套餐'
       })
     } else if (score > 160 && score <= 200) {
       this.setData({
         grade: 3,
-        gradeName: '三年级'
+        gradeName: '三年级',
+        interfereType: '需中度干预',
+        recommendedPackage: '自闭症三年级服务套餐'
       })
     } else if (score > 200 && score <= 220) {
       this.setData({
         grade: 4,
-        gradeName: '四年级'
+        gradeName: '四年级',
+        interfereType: '需轻度干预',
+        recommendedPackage: '自闭症四年级服务套餐'
       })
     } else if (score > 220 && score <= 240) {
       this.setData({
         grade: 5,
-        gradeName: '五年级'
+        gradeName: '五年级',
+        interfereType: '需轻度干预',
+        recommendedPackage: '自闭症五年级服务套餐'
       })
     }
   },
