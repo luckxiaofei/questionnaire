386c6a3d41d1745b2dc0130823169f2ea8b534e0
chenfei <luckxiaofei@outlook.com>
2020-04-23 14:16:33 +0800

    1.0

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,14 +13,17 @@ Page({
 
   },
 
-  onClick(){
-    Dialog.alert({
-      title: '温馨提示',
-      message: '亲爱的家长，您好！欢迎您使用圣之星自闭症评估小程序。本程序中的评估内容只适用于自闭症儿童，请您在确认儿童为自闭症后进行评估。'
-    }).then(() => {
-     wx.navigateTo({
-       url: '/pages/questionnaire/questionnaire',
-     })
-    });
+  onClick() {
+    // Dialog.alert({
+    //   title: '温馨提示',
+    //   message: '亲爱的家长，您好！欢迎您使用圣之星自闭症评估小程序。本程序中的评估内容只适用于自闭症儿童，请您在确认儿童为自闭症后进行评估。'
+    // }).then(() => {
+    //  wx.navigateTo({
+    //    url: '/pages/questionnaire/questionnaire',
+    //  })
+    // });
+    wx.navigateTo({
+      url: '/pages/questionnaire/questionnaire',
+    })
   }
 })
